bc5cf6add44f1fdd56b5e3f67e018072e28d9d3c
HungJen Liao <hjliao0705@gmail.com>
2023-03-14 22:57:54 +0800

    hide secret key

--- MapProject2/settings.py
+++ MapProject2/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5+m=$44cokks7efjz(4(ruc0-==^w*5ov6umml)#we#4ns+7+b'
+SECRET_KEY = '**************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
